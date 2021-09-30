4b9cb4ecc177cf48ae54953614906b28d68aee2c
TenkoTans <85549470+TenkoTans@users.noreply.github.com>
2021-10-01 02:24:31 +1000

    Update index.py

--- index.py
+++ index.py
@@ -21,6 +21,69 @@ bot.remove_command('help')
 async def on_ready():
     print(f'Logged in as: {bot.user.name}')
 
+@bot.event
+async def on_raw_reaction_add(payload):
+    msg_id = payload.message_id
+
+    with open("selfrole.json", "r") as f:
+        self_roles = json.load(f)
+
+    if payload.member.bot:
+        return
+    
+    if str(msg_id) in self_roles:
+        emojis = []
+        roles = []
+
+        for emoji in self_roles[str(msg_id)]['emojis']:
+            emojis.append(emoji)
+
+        for role in self_roles[str(msg_id)]['roles']:
+            roles.append(role)
+        
+        guild = bot.get_guild(payload.guild_id)
+
+        for i in range(len(emojis)):
+            choosed_emoji = str(payload.emoji)
+            if choosed_emoji == emojis[i]:
+                selected_role = roles[i]
+
+                role = discord.utils.get(guild.roles, name=selected_role)
+
+                await payload.member.add_roles(role)
+                await payload.member.send(f"**{guild.name}** : You have been given the {selected_role} role!")
+
+@bot.event
+async def on_raw_reaction_remove(payload):
+    msg_id = payload.message_id
+
+    with open("selfrole.json", "r") as f:
+        self_roles = json.load(f)
+    
+    if str(msg_id) in self_roles:
+        emojis = []
+        roles = []
+
+        for emoji in self_roles[str(msg_id)]['emojis']:
+            emojis.append(
+                emoji)
+
+        for role in self_roles[str(msg_id)]['roles']:
+            roles.append(role)
+        
+        guild = bot.get_guild(payload.guild_id)
+
+        for i in range(len(emojis)):
+            choosed_emoji = str(payload.emoji)
+            if choosed_emoji == emojis[i]:
+                selected_role = roles[i]
+
+                role = discord.utils.get(guild.roles, name=selected_role)
+
+                member = await(guild.fetch_member(payload.user_id))
+                if member is not None:
+                    await member.remove_roles(role)
+
 #extra commands
 @bot.command(pass_context=True, aliases=['setprefix'])
 @commands.has_permissions(administrator = True)
